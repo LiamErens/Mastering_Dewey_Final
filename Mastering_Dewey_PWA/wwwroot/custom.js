42bc30314b06c8a70a52ee529da864da2fedda2c
Liam Erens <ST10085392@vcconnect.edu.za>
2023-09-22 11:33:45 +0200

    Add project files.

---
+++ Mastering_Dewey_PWA/wwwroot/custom.js
@@ -0,0 +1,8 @@
+﻿window.blazorCheckPositions = () => {
+    const listItems = document.querySelectorAll('.dropList');
+    const order = [];
+    listItems.forEach(item => {
+        order.push(item.innerText);
+    });
+    return order;
+}
